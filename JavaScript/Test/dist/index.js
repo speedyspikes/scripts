2f5ab8a5874e597c264e18d4b6a362a845760f9f
speedyspikes <speedyspikes@gmail.com>
2025-05-15 11:51:58 -0600

    changes

---
+++ JavaScript/Test/dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// import modules
+// caution: `eventLoop` HAS to be imported before `gui`, and `gui` HAS to be
+// imported before any `gui` submodules.
+const eventLoop = require("@next-flip/fz-sdk-mntm/event_loop");
+const gui = require("@next-flip/fz-sdk-mntm/gui");
+const dialog = require("@next-flip/fz-sdk-mntm/gui/dialog");
+const badusb = require("@next-flip/fz-sdk-mntm/badusb");
+// a common pattern is to declare all the views that your app uses on one object
+const views = {
+    dialog: dialog.makeWith({
+        header: "Hello from Test",
+        text: "Check out index.ts and\nchange something :)",
+        center: "Gonna do that!",
+    }),
+};
+// stop app on center button press
+eventLoop.subscribe(views.dialog.input, (_sub, button, eventLoop) => {
+    if (button === "center")
+        eventLoop.stop();
+}, eventLoop);
+// stop app on back button press
+eventLoop.subscribe(gui.viewDispatcher.navigation, (_sub, _item, eventLoop) => {
+    eventLoop.stop();
+}, eventLoop);
+// run app
+gui.viewDispatcher.switchTo(views.dialog);
+eventLoop.run();
