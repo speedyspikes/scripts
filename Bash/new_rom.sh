400ca7f3a394db73f77bdf4264395099e64d666b
speedyspikes <speedyspikes@gmail.com>
2026-02-21 22:59:28 -0700

    starting again

--- Bash/new_rom.sh
+++ Bash/new_rom.sh
@@ -27,18 +27,14 @@ while true; do
     name=$(basename "$f")
     if [ -d "$f" ]; then
       printf "%3d) ${COL_DIR}%s${COL_RST}\n" "$i" "$name"
-      type="dir"
     elif [[ "$f" == *.zip ]]; then
       printf "%3d) ${COL_ZIP}%s${COL_RST}\n" "$i" "$name"
-      type="zip"
     elif [[ "$f" == *.iso || "$f" == *.wbfs ]]; then
       printf "%3d) ${COL_FILE}%s${COL_RST}\n" "$i" "$name"
-      type="file"
     else
       printf "%3d) %s\n" "$i" "$name"
-      type="file"
     fi
-    entries+=("$f$'\t'$type")
+    entries+=("$f")
   done
   shopt -u nullglob
 
@@ -47,81 +43,20 @@ while true; do
     exit 0
   fi
 
+  # Prompt user for selection
   echo
-  read -rp $'Select a number, or type a path (blank to exit): ' sel
-  if [ -z "$sel" ]; then
-    echo "No selection made. Exiting."
-    exit 0
-  fi
-
-  selected=""
-  if [[ "$sel" =~ ^[0-9]+$ ]]; then
-    num=$((sel-1))
-    if [ "$num" -lt 0 ] || [ "$num" -ge "${#items[@]}" ]; then
-      echo "Invalid selection number. Try again."
-      continue
+  while true; do
+    read -rp $'Select a number (blank to exit): ' sel
+    if [ -z "$sel" ]; then
+      echo "No selection made. Exiting."
+      exit 0
     fi
-    selected="${items[$num]}"
-    sel_type="${types[$num]}"
-  else
-    # treat input as filepath
-    if [ -e "$sel" ]; then
-      selected="$sel"
-      if [ -d "$sel" ]; then
-        sel_type="dir"
-      else
-        sel_type="file"
-      fi
+    if (( sel >= 1 && sel <= i )); then
+      break
     else
-      echo "Provided path does not exist: $sel"
-      continue
+      echo "Invalid selection. Please try again."
     fi
-  fi
-
-  if [ "$sel_type" = "dir" ]; then
-    read -rp $'Selected a folder. Type "m" to move, "g" to go into it, or blank to cancel: ' op
-    if [ -z "$op" ]; then
-      echo "Canceled."
-      continue
-    fi
-    if [[ "$op" =~ ^[Gg]$ ]]; then
-      CURRENT_DIR="$selected"
-      continue
-    elif [[ "$op" =~ ^[Mm]$ ]]; then
-      to_move="$selected"
-    else
-      echo "Unknown option. Returning to list."
-      continue
-    fi
-  else
-    to_move="$selected"
-  fi
-
-  echo
-  echo "Choose destination:"
-  echo " 1) gamecube"
-  echo " 2) wii"
-  echo " 3) wiiu"
-  read -rp $'Enter number or provide a custom path: ' destsel
-
-  if [[ "$destsel" =~ ^[0-9]+$ ]]; then
-    case "$destsel" in
-      1) dest="$DEST_ROOT/gamecube" ;;
-      2) dest="$DEST_ROOT/wii" ;;
-      3) dest="$DEST_ROOT/wiiu" ;;
-      *) echo "Invalid number. Move canceled."; continue ;;
-    esac
-  else
-    if [ -z "$destsel" ]; then
-      echo "No destination provided. Move canceled."
-      continue
-    fi
-    dest="$destsel"
-  fi
-
-  mkdir -p "$dest"
-  mv -v "$to_move" "$dest/"
-  echo "Moved '$(basename "$to_move")' to '$dest'"
-
+  done
+  echo ${entries[$sel-1]}
   # continue the loop (repeat)
 done
